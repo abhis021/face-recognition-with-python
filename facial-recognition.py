5d54b9db2d2059e3b2a0925932b0476e1a23b2a9
ABHISHEK UPADHYAY <abhisheku722@protonmail.com>
2021-04-22 23:42:23 +0530

    Successful in Live Video Capture.

--- facial-recognition.py
+++ facial-recognition.py
@@ -1,3 +1,13 @@
-import face_recognition
-import os
 import cv2
+import numpy as np
+
+cam = cv2.VideoCapture(0)
+
+while True:
+    b, img = cam.read()
+    if b:
+        cv2.imshow("Face Detection", img)
+        cv2.waitKey(1)
+    else:
+        print("The camera is not working.")
+        break
